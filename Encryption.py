3f3341404772bca25b0e0547100f01e6f8073b0e
bringerofsingularity <89136808+bringerofsingularity@users.noreply.github.com>
2022-11-16 13:17:44 +0530

    Add files via upload

---
+++ Encryption.py
@@ -0,0 +1,21 @@
+import wave
+
+song = wave.open("song.wav", mode = 'rb')
+
+frame_bytes = bytearray(list(song.readframes(song.getnframe())))
+
+string = input("Enter Your Message")
+
+string = string + int((len(frame_bytes) - (len(string)*8*8))/8) * '#'
+
+bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8, '0') for i in string])))
+
+for i, bit in enumerate(bits):
+    frame_bytes[i] = (frame_bytes[i] & 254) | bit
+
+frame_modified = bytes(frame_bytes)
+
+with wave.open('song_embeddeb.wav', 'wn') as fd:
+    fd.setparams(song.getparams())
+    fd.writeframes(frame_modified)
+song.close()
